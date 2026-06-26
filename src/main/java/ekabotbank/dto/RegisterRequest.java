6f6fcd75669a190b58b8d6f1d997859dfb57e789
ekabotdev <ekabotdev@gmail.com>
2026-06-26 19:49:39 +0300

    Add register request

---
+++ src/main/java/ekabotbank/dto/RegisterRequest.java
@@ -0,0 +1,20 @@
+package ekabotbank.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
