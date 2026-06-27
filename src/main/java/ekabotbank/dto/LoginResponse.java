d18a9ed7fd6e7ddb9ac05eec84214d9eb8373b41
ekabotdev <ekabotdev@gmail.com>
2026-06-27 12:05:00 +0300

    Update login response class

--- src/main/java/ekabotbank/dto/LoginResponse.java
+++ src/main/java/ekabotbank/dto/LoginResponse.java
@@ -1,10 +1,14 @@
 package ekabotbank.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
 
     private String message;
